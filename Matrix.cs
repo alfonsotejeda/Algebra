dfe0ec194c5361192d5193f25d84e50234ceaf49
Alfonso <129644324+alfonsotejeda@users.noreply.github.com>
2025-01-07 14:20:34 -0500

    Refactor determinant calculation and add adjoint matrix functionality

--- Matrix.cs
+++ Matrix.cs
@@ -34,7 +34,7 @@ namespace Algera.Matrix
             }
         }
 
-        public double CalculateTheDeterminant(double [,] matrix)
+        public double CalculateTheDeterminant(double [,] matrix , int row , int column)
         {
             if (matrix.GetLength(0) != matrix.GetLength(1))
             {
@@ -49,36 +49,83 @@ namespace Algera.Matrix
                 return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
             }
             double[] determinat = new double[matrix.GetLength(1)];
-            for(int i = 0 ; i < matrix.GetLength(1);i++)
+            for(int i = column ; i < matrix.GetLength(1);i++)
             {
-                determinat[i] = Math.Pow(-1,i) * matrix[i,0] * CalculateTheDeterminant(GetCofactors(matrix , i , 0));
+                determinat[i] = Math.Pow(-1,i) * matrix[i,0] * CalculateTheDeterminant(GetMinors(matrix , i , column) , row , column);
             }
             return determinat.Sum();
         }
-        private double[,] GetCofactors(double[,] matrix, int row, int column)
+        private double[,] GetMinors(double[,] matrix, int elementrow, int elementcolumn)
         {
             int rows = matrix.GetLength(0) - 1;
             int columns =  matrix.GetLength(1) - 1;
-            double[,] newMatrix = new double[rows,columns];
+            double[,] minorsMatrix = new double[rows,columns];
             for (int i = 0; i < rows+1; i++)
             {
-                if (i == row)
+                if (i == elementrow)
                 {
                     continue;
                 }
-                for (int j = 0; j < columns+1; j++)
+                for (int j = 0; j < columns+1; j++) 
                 {
-                    if(j == column)
+                    if(j == elementcolumn)
                     {
                         continue;
                     }
-                    newMatrix[i > row ? i - 1 : i, j > column ? j - 1 : j] = matrix[i, j];
+                    minorsMatrix[i > elementrow ? i - 1 : i, j > elementcolumn ? j - 1 : j] = matrix[i, j];
                 }
                 
             }
-            return newMatrix;
+            return minorsMatrix;
         }
-    }
-    
-    
+        
+        public double[,] CalculateAdjointMatrix(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns =  matrix.GetLength(1);
+            double[,] cofactorsMatrix = new double[rows , columns];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    cofactorsMatrix[i,j] = Math.Pow(-1,i+j) * CalculateTheDeterminantAuxiliarForAdjoin(matrix , i , j);
+                }
+            }
+            double[,] adjoinMatrix = TransposeMatrix(cofactorsMatrix);
+            return adjoinMatrix;
+        }
+        private double[,] TransposeMatrix(double[,] matrix)
+        {
+            double[,] transposeMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    transposeMatrix[i, j] = matrix[j, i];
+                }
+            }
+            return transposeMatrix;
+        }
+        private double CalculateTheDeterminantAuxiliarForAdjoin(double [,] matrix , int row , int column)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1))
+            {
+                throw new ArgumentException("The matrix must be square");
+            }
+            if (matrix.GetLength(0) == 1)
+            {
+                return matrix[0, 0];
+            }
+            if (matrix.GetLength(0) == 2)
+            {
+                return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            }
+            double minorDeterminat = 0;
+            
+            minorDeterminat = CalculateTheDeterminantAuxiliarForAdjoin(GetMinors(matrix , row , column) , row , column);
+            
+            return minorDeterminat;
+        }
+    }    
 }
--- Program.cs
+++ Program.cs
@@ -5,10 +5,21 @@ namespace Main
     {
         public static void Main(string[] args)
         {
-            Matrix matrix = new Matrix(4, 4);
+            Matrix matrix = new Matrix(3, 3);
             matrix.GetMatrix();
-            double determinant = matrix.CalculateTheDeterminant(matrix.matrix);
+            double determinant = matrix.CalculateTheDeterminant(matrix.matrix , 0 , 0);
+            double[,] adjoinMatrix = matrix.CalculateAdjointMatrix(matrix.matrix);
+
             Console.WriteLine($"The determinant of the matrix is {determinant}");
+            Console.WriteLine("The adjoint matrix is : ");
+            for (int i = 0; i < adjoinMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < adjoinMatrix.GetLength(1); j++)
+                {
+                    Console.Write(adjoinMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
